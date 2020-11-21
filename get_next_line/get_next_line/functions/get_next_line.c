8f6cbe5873c62376673c715e04259a270d4aa776
ydemura <ydemura@student.codam.nl>
2020-11-21 21:09:56 +0100

    3

--- get_next_line/get_next_line/functions/get_next_line.c
+++ get_next_line/get_next_line/functions/get_next_line.c
@@ -16,20 +16,22 @@
 //Parameters 	#1. file descriptor for reading
 //				#2. The value of what has been read
 
-void free_it(char *str)
-{
-	free(str);
-}
-
 static char *malloc_it(char *str, int len)
 {
-	free_it(str);
-	str = malloc(len * sizeof(char));
-	if (str == NULL)
+	char *new_str;
+	new_str = malloc((len + 1) * sizeof(char));
+	if (new_str == NULL)
 	{
 		return (0);
 	}
-	return (str);
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	//free(str);
+	return (new_str);
 }
 
 
@@ -38,21 +40,21 @@ int		get_next_line(int fd, char **line)
 	size_t res = 0;
 //	size_t len = 0;
 	char *str = NULL;
+	char *temp = NULL;
 	
 	int len = 1;
 	int i = 0;
-	str = malloc_it(str, len);
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return (0);
+	}
 	
 	while ((res = read(fd, str, BUFFER_SIZE))) //if not error of EOF it is == 1(true)
 	{
 		*line = str;
 		//printf("%s\n", str);
 		//printf("%s", *line);
-		if (i >= len)
-		{
-			len = len * 2;
-			str = malloc_it(str, len);
-		}
 		if ((unsigned char)*line == '\n')
 		{
 			return ((int)res);
@@ -60,6 +62,15 @@ int		get_next_line(int fd, char **line)
 		line++;
 		str++;
 		i++;
+		if (i >= len)
+		{
+			len = len * 2;
+			printf("%s\n", str);
+			str[i] = '\0';
+			temp = malloc_it(str, len);
+			free(str);
+			str = temp;
+		}
 	}
 	
 	return ((int)res);
