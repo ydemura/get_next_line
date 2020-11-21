702a79cdb48ac2691c96aae183b125744ba4e647
ydemura <ydemura@student.codam.nl>
2020-11-21 15:39:34 +0100

    start.......................................

---
+++ get_next_line/get_next_line/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  get_next_line
+//
+//  Created by Yuliia Demura on 11/21/20.
+//  Copyright © 2020 Yuliia Demura. All rights reserved.
+//
+
+#include <stdio.h>
+#include "get_next_line.h"
+#include "test_get_next_line.h"
+
+int main() //int argc, const char * argv[]
+{................
+	// insert code here...
+	test_get_next_line();
+	
+	return 0;
+}
