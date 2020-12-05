625dd83127eb4193888889f7f45fcd645a487430
ydemura <ydemura@student.codam.nl>
2020-12-05 14:28:45 +0100

    before first eval

---
+++ get_next_line/get_next_line/before_first_eval/old_get_next_line copy.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ydemura <ydemura@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/21 13:12:43 by ydemura       #+#    #+#                 */
+/*   Updated: 2020/11/21 13:12:49 by ydemura       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		cut_line_and_left(char **line, t_memory *memory, int n, char *temp)
+{
+	while (temp[n] != '\0')
+	{
+		if (temp[n] == '\n')
+		{
+			*line = ft_strdup_till_n(temp, n);
+			after_n_memcpy(memory->left, temp, n);
+			free(temp);
+			return (1);
+		}
+		n++;
+	}
+	memory->counter = n;
+	free(temp);
+	return (0);
+}
+
+int		ft_search_end_of_line(char **line, t_memory *memory)
+{
+	char			*temp;
+	unsigned int	n;
+
+	n = 0;
+	if (*line != NULL)
+		temp = ft_strjoin(*line, memory->left);
+	else
+		temp = ft_strdup(memory->left);
+	if (memory->counter != 0)
+		n = memory->counter - 1;
+	if (cut_line_and_left(line, memory, n, temp) == 1)
+	{
+		memory->counter = 0;
+		return (1);
+	}
+	return (0);
+}
+
+int		ft_read(int fd, t_memory *memory, char **line)
+{
+	memory->res = (int)read(fd, memory->left, BUFFER_SIZE);
+	if (memory->res == -1)
+		return (-1);
+	memory->left[memory->res] = '\0';
+	if (memory->res == 0)
+		*line = ft_strjoin(*line, memory->left);
+	return (memory->res);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static	t_memory memory;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (-1);
+	memory.res = 1;
+	*line = NULL;
+	while (memory.res > 0)
+	{
+		if (*(memory.left))
+		{
+			if (ft_search_end_of_line(line, &memory) == 1)
+				return (1);
+			else
+			{
+				*line = ft_strjoin(*line, memory.left);
+				*(memory.left) = '\0';
+			}
+		}
+		else if ((ft_read(fd, &memory, line)) == -1)
+			return (-1);
+	}
+	return (0);
+}
---
+++ get_next_line/get_next_line/functions/old_get_next_line.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ydemura <ydemura@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/21 13:12:46 by ydemura       #+#    #+#                 */
+/*   Updated: 2020/11/21 13:12:47 by ydemura       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+
+# if BUFFER_SIZE < 0
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct		s_memory
+{
+	unsigned int	counter;
+	int				res;
+	char			left[BUFFER_SIZE + 1];
+}					t_memory;
+
+int					ft_strlen(const char *str);
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strdup(const char *s1);
+char				*ft_strdup_till_n(const char *s1, int len);
+void				after_n_memcpy(char *left, char *temp, unsigned int n);
+void				clean_string(t_memory *memory);
+int					cut_line_and_left(char **line, t_memory *memory, int n,
+	char *temp);
+int					ft_search_end_of_line(char **line, t_memory *memory);
+int					ft_read(int fd, t_memory *memory, char **line);
+int					get_next_line(int fd, char **line);
+
+#endif
